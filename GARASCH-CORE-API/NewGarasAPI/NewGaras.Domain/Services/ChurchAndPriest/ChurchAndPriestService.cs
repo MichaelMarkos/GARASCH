ea60a19bde5a973f3496f38783541ecf846c4415
PatricVeta <patric.vetanoia@gmail.com>
2025-06-19 12:26:13 +0300

    make a service and its interface for chruch and priest

---
+++ GARASCH-CORE-API/NewGarasAPI/NewGaras.Domain/Services/ChurchAndPriest/ChurchAndPriestService.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using NewGaras.Domain.Models;
+using NewGaras.Infrastructure;
+using NewGaras.Infrastructure.Entities;
+using NewGaras.Infrastructure.Interfaces.ServicesInterfaces.Church;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewGaras.Domain.Services.ChurchAndPriest
+{
+    public class ChurchAndPriestService : IChurchAndPriestService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private HearderVaidatorOutput validation;
+        public HearderVaidatorOutput Validation
+        {
+            get
+            {
+                return validation;
+            }
+            set
+            {
+                validation = value;
+            }
+        }
+        public ChurchAndPriestService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public BaseResponseWithId<long> AddNewPriest(string PriestName)
+        {
+            var response = new BaseResponseWithId<long>()
+            {
+                Errors = new List<Error>(),
+                Result = true
+            };
+
+
+
+            try
+            {
+                //var newFamilyStatus = new FamilyStatus()
+                //{
+                //    StatusName = dto.StatusName,
+                //    Description = dto.Description,
+                //};
+                //_unitOfWork.FamilyStatus.Add(newFamilyStatus);
+                //_unitOfWork.Complete();
+
+                //response.ID = newFamilyStatus.Id;
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Result = false;
+                Error err = new Error();
+                err.ErrorCode = "E-1";
+                err.errorMSG = "Exception :" + ex.Message;
+                response.Errors.Add(err);
+                return response;
+            }
+        }
+    }
+}
