305254393b6610a8cced094cadcb559956174059
GergesVeta <gerges.abdallah@vetanoia.com>
2025-07-01 16:15:51 +0300

    GetChurchesAndPriestOfHrUser Retrieved

---
+++ GARASCH-CORE-API/NewGarasAPI/NewGaras.Infrastructure/DTO/HrUser/GetChurchesAndPriestsToHrUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewGaras.Infrastructure.DTO.HrUser
+{
+    public class GetChurchesAndPriestsToHrUser
+    {
+        public long HrUserId { get; set; }
+        public long? ChurchOfPresenceId { get; set; }
+        public string ChurchOfPresenceName { get; set; }
+        public long? BelongToChurchId { get; set; }
+        public string BelongToChurchName { get; set; }
+        public long? PriestId { get; set; }
+        public string PriestName { get; set; }
+    }
+}
